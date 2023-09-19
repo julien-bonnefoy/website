6100289fc10ee58d473d255fde0b9dde63b4ad9d
julien-bonnefoy <julien.bonnefoy1@orange.fr>
2023-09-19 20:23:06 +0200

    update

--- application/__init__.py
+++ application/__init__.py
@@ -96,7 +96,7 @@ def create_flask_server():
     :param config_object: The configuration object to use.
     """
     server = Flask(__name__)
-    server.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////home/julien/website/application/data/biocodex.db"
+    server.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///application/data/biocodex.db"
     server.config["SECRET_KEY"] = "93807f0a7b2e5577c4e3b4f93e70ad4de7ecc9c6d0262652d3a47be8700379e0"
 
     bootstrap.init_app(server)
